26d108328928d2cef30015d4fccf2302a363733f
Karman <revolutioncse@gmail.com>
2020-07-04 10:41:05 -0400

    changes

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,13 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Controller  extends SpringBootServletInitializer {
+public class Controller  extends org.springframework.boot.web.support.SpringBootServletInitializer {
    @Override
    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
       return application.sources(Controller.class);
@@ -20,6 +19,6 @@ public class Controller  extends SpringBootServletInitializer {
    
    @RequestMapping(value = "/")
    public String hello() {
-      return "Hello World from Tomcat";
+      return "Hello World";
    }
 }
