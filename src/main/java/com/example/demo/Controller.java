daf64dc60e3e152d510c23c87d84dd7ab376d1a7
Karman <revolutioncse@gmail.com>
2020-07-03 21:31:25 -0400

    commit on master

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Controller  extends SpringBootServletInitializer {
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(Controller.class);
+   }
+   public static void main(String[] args) {
+      SpringApplication.run(Controller.class, args);
+   }
+   
+   @RequestMapping(value = "/")
+   public String hello() {
+      return "Hello World from Tomcat";
+   }
+}
